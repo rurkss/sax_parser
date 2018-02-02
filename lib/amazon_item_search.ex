58b37bc16ac88cbbad68e22eb531462e86dae7c9
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2018-02-02 15:06:14 +0200

    initial commit

---
+++ lib/amazon_item_search.ex
@@ -0,0 +1,18 @@
+defmodule AmazonItemSearch do
+  @moduledoc """
+  Documentation for AmazonItemSearch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AmazonItemSearch.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/amazon_item_search_test.exs
@@ -0,0 +1,8 @@
+defmodule AmazonItemSearchTest do
+  use ExUnit.Case
+  doctest AmazonItemSearch
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
